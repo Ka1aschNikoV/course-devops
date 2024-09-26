178e4e7ce47f9dc93ddb100a18562a8c1b18787d
dpniva <niko.vakiparta@tuni.fi>
2024-09-26 22:19:22 +0300

    added instructions

--- service2/service2/src/main/java/service2/App.java
+++ service2/service2/src/main/java/service2/App.java
@@ -7,7 +7,7 @@ import java.net.InetSocketAddress;
 public class App {
     public static void main(String[] args) throws IOException {
         
-        HttpServer server = HttpServer.create(new InetSocketAddress("localhost", 3002), 0);
+        HttpServer server = HttpServer.create(new InetSocketAddress("localhost", 8200), 0);
         server.start();
         System.out.println("Service2 Listening on port 3002");
 
