e92629410e58eae702d90b4f25bd83833ce09858
dpniva <niko.vakiparta@tuni.fi>
2025-01-23 09:36:23 +0200

    what

--- test/index.js
+++ test/index.js
@@ -1,8 +1,8 @@
 import express from "express"
 const app = express();
-const port = 8194;
+const port = 8193;
 
-app.listen(8194, async () => {
+app.listen(8193, async () => {
     console.log(`Listening on port ${port}!`);
   
 });
